18739d968cf712298fa3f94f6dfd16b51595b3db
Matheus Dornelles <mdmaia91@gmail.com>
2023-02-12 11:24:08 -0300

    GET finished

--- src/main/java/com/example/demo/domain/Categoria.java
+++ src/main/java/com/example/demo/domain/Categoria.java
@@ -1,14 +1,20 @@
+
 package com.example.demo.domain;
+
 
 import java.io.Serializable;
 import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
-
+@Table(name = "categoria" )
+@JsonIgnoreProperties({"hibernateLazyInitializer","handler"})
 @Entity
 public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
